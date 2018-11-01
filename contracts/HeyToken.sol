08dba4c1bada762ec9cd1f6070a8de564a9cbf77
thomasvds <vanderstraeten.thomas@gmail.com>
2018-11-01 10:11:48 +0100

    First pass of Solium

--- contracts/HeyToken.sol
+++ contracts/HeyToken.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.4.24;
 import "./token/ERC20.sol";
 
 /**
- * @title SimpleToken
- * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
- * Note they can later distribute these tokens as they wish using `transfer` and other
- * `ERC20` functions.
- * This code is litterally a copy-paste of OpenZeppelin's Simple Token:
- * https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/examples/SimpleToken.sol
- */
+* @title SimpleToken
+* @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+* Note they can later distribute these tokens as they wish using `transfer` and other
+* `ERC20` functions.
+* This code is litterally a copy-paste of OpenZeppelin's Simple Token:
+* https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/examples/SimpleToken.sol
+*/
 contract HeyToken is ERC20 {
 
   string public constant name = "HeyToken";
@@ -19,8 +19,8 @@ contract HeyToken is ERC20 {
   uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(decimals)); // 1 billion tokens
 
   /**
-   * @dev Constructor that gives msg.sender all of existing tokens.
-   */
+  * @dev Constructor that gives msg.sender all of existing tokens.
+  */
   constructor() public {
     _mint(msg.sender, INITIAL_SUPPLY);
   }
