a5693265b8b42a1a49c6b93d56d416cc3f46ae71
thomasvds <vanderstraeten.thomas@gmail.com>
2018-11-01 13:28:25 +0100

    Add drain and validDestination features

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -20,11 +20,59 @@ contract Token is ERC20, EmergencyERC20Drain {
     uint256 public constant INITIAL_SUPPLY = 1000000000 * (10 ** uint256(decimals)); // 1 billion tokens
 
     /**
+    * @dev From Consensys recommendations:
+    * https://consensys.github.io/smart-contract-best-practices/tokens/
+    * Prevent sending of the token to invalid addresses.
+    */
+    modifier validDestination(
+        address to
+    )
+    {
+        require(to != address(0));
+        require(to != address(this));
+        _;
+    }
+
+    /**
     * @dev Constructor that gives msg.sender all of existing tokens.
     */
     constructor()
         public
     {
         _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+
+    /**
+    * @dev From Consensys recommendations:
+    * https://consensys.github.io/smart-contract-best-practices/tokens/
+    * Overrides the standard ERC20 transfer to apply the validDestination
+    * modifier. All else remains the same.
+    */
+    function transfer(
+        address to,
+        uint value
+    )
+        validDestination(to)
+        returns (bool)
+    {
+        return super.transfer(to, value);
+    }
+
+    /**
+    * @dev From Consensys recommendations:
+    * https://consensys.github.io/smart-contract-best-practices/tokens/
+    * Overrides the standard ERC20 transferFrom to apply
+    * the validDestination modifier. All else remains the same.
+    */
+    function transferFrom(
+        address from,
+        address to,
+        uint value
+    )
+        validDestination(to)
+        returns (bool)
+    {
+        return super.transferFrom(from, to, value);
     }
 }
