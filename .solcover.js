13fcbd80bdb82946d94956326fdb593bbd120631
thomasvds <vanderstraeten.thomas@gmail.com>
2018-11-02 11:27:52 +0100

    Fix solidity coverage gfas issues

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,6 @@
 module.exports = {
   copyPackages: ['openzeppelin-solidity'],
   skipFiles: ['mocks/ERC20Mock.sol'],
-  testCommand: 'npm run test',
+  // Do not use the test network here as solidity-coverage has gas issues with it
+  testCommand: 'truffle test test/Token.test.js test/TokenSale.test.js test/Gateway.test.js',
 }
