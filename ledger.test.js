01c41f691df61f0a2269b75688f66d5262928d04
thomasvds <vanderstraeten.thomas@gmail.com>
2018-11-30 11:18:21 +0100

    Improve Ledger Nano S testing script

--- ledger.test.js
+++ ledger.test.js
@@ -1,8 +1,13 @@
 // A simple script to verify connectivity with and addresses of a Ledger Nano S.
+// Note that this requires an Infura API key saved in an .infura file.
 
 require('babel-polyfill');
 const Web3 = require('web3');
-const LedgerWalletProvider = require('truffle-ledger-provider');
+
+// Watchout for the very similar names of these packages when looking them up
+// on GitHub etc ;)
+const bip39ProviderFactory = require('truffle-ledger-provider');
+const bip44ProviderFactory = require('truffle-ledger-wallet-provider');
 
 const DERIVATION_PATH =  "44'/60'/0'/0";
 
@@ -12,14 +17,14 @@ const INFURA_ENDPOINT = `https://ropsten.infura.io/v3/${INFURA_API_KEY}`;
 // HOW-TO:
 // 0. Go through the readme below to understand derivation path subtleties
 // 1. Connect your Ledger Nano S
-// 2. Input the PIN
+// 2. Input the device PIN to unlock it
 // 3. Open the Ethereum app
 // 4. Enable the Contract and Display modes from Settings
 // 5. Make sure you've got Ropsten ETH on your account to send a transaction
 // 6. Enjoy!
 
 // An important note on derivation paths:
-// In implementation A and on the Ledger Ethereum Chrome Wallet, the default
+// In implementation A and on the Ledger Ethereum Chrome Wallet, the default BIP39
 // key derivation path is 44'/60'/0'/0. On Ledger Live however, an additional zero
 // is added to the path as per the BIP44 recommendation. See the full description here:
 // https://github.com/MyCryptoHQ/MyCrypto/issues/2070
@@ -28,53 +33,52 @@ const INFURA_ENDPOINT = `https://ropsten.infura.io/v3/${INFURA_API_KEY}`;
 // Live addresses, since they won't be usable from Web3. Again, all these comments
 // apply only to implementation A, and are not relevant for B which uses BIP44.
 
-async function test() {
-  // IMPLEMENTATION A (not BIP44, not working with Truffle)
-  // const provider = new LedgerWalletProvider(
-  //   {
-  //     networkId: 3, // Ropsten testnet
-  //     path: DERIVATION_PATH,
-  //     accountsOffset: 0, // Start from first address to derive accounts
-  //     accountsLength: 5, // Load 5 accounts
-  //   },
-  //   INFURA_ENDPOINT
-  // );
+const testTransaction = {
+  from: accounts[0],
+  gasPrice: '20000000000',
+  gas: '21000',
+  to: '0x3535353535353535353535353535353535353535',
+  value: '100000000',
+  data: ''
+};
 
-  // IMPLEMENTATION B (BIP 44, uses 0x's implementation, works with Truffle)
-  const provider = require("truffle-ledger-wallet-provider").default(
+function bip39Provider() {
+  // IMPLEMENTATION A (not working with Truffle)
+  return new bip39ProviderFactory(
+    {
+      networkId: 3, // Ropsten testnet
+      path: DERIVATION_PATH,
+      accountsOffset: 0, // Start from first address to derive accounts
+      accountsLength: 5, // Load 5 accounts
+    },
+    INFURA_ENDPOINT
+  );
+};
+
+function bip44Provider() {
+  // IMPLEMENTATION B (uses 0x's implementation, works with Truffle)
+  return bip44ProviderFactory.default(
     INFURA_ENDPOINT,
     3,
-    DERIVATION_PATH,
+    DERIVATION_PATH, // Strangely we have to use BIP39 derivation path in argument
   );
+};
 
+async function test() {
+  const provider = bip44Provider();
   const web3 = new Web3(provider);
-  const accounts = await web3.eth.getAccounts();
 
   // List the accounts available, derived from default path.
+  const accounts = await web3.eth.getAccounts();
   console.log(accounts)
 
   // Trial transaction to test Ledger connectivity
-  const signature = await web3.eth.signTransaction({
-    from: accounts[0],
-    gasPrice: '20000000000',
-    gas: '21000',
-    to: '0x3535353535353535353535353535353535353535',
-    value: '100000000',
-    data: ''
-  });
-
+  const signature = await web3.eth.signTransaction(testTransaction);
   console.log(signature);
 
   // Trial transaction to test full connectivity to Ropsten.
   try {
-    const txHash = await web3.eth.sendTransaction({
-      from: accounts[0],
-      gasPrice: '20000000000',
-      gas: '21000',
-      to: '0x3535353535353535353535353535353535353535',
-      value: '100000000',
-      data: ''
-    });
+    const txHash = await web3.eth.sendTransaction(testTransaction);
   } catch (error) {
     // This is a known limitation when using Infura, and does not prevent the tx.
     // See issue here: https://github.com/ethereum/web3.js/issues/951
